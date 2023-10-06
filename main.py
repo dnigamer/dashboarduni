170753c2d40914a6ba85086e240d1c1b064ac90e
Gonçalo M <dnigamerofficial@gmail.com>
2023-10-06 19:41:21 +0100

    Added edit tab on html fully. Redone things.
    - Implemented fetch records API endpoint and respective tab dashboard on index.html.
    - Created /api/consulta to get all the records in a specific date given in JSON format.
    - Created /api/editar/{id} (ENDPOINT TO BE REDESIGNED) but without any logic behind it yet.
    - Redone DB, once again, to only have the value instead of 2 different rows one for credit or debit and contain an "type" row on it to specify if its debit or credit.
    - Adapted saldo API endpoint to this DB change and redone saldo calculator to be more efficient.
    - Updated tabela.sql with new table modifications and other useful functions to be used when needed.
    
    TODO: Adapt add record endpoint and dashboard to new DB scheme.

--- main.py
+++ main.py
@@ -50,8 +50,21 @@ async def root(request: Request):
 
 @app.get("/api/saldo", response_class=JSONResponse)
 async def saldo_api():
-    cursor.execute("SELECT saldo FROM registos ORDER BY id DESC LIMIT 1")
-    saldo: float = cursor.fetchone()[0]
+    try:
+        cursor.execute(
+            "SELECT ROUND(ABS(SUM(CASE WHEN tipo = 1 THEN valor ELSE 0 END) - SUM(CASE WHEN tipo = 2 THEN valor ELSE 0 END)), 2) AS saldo FROM registos;"
+        )
+        saldo: float = cursor.fetchone()[0]
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "httpCode": 500,
+                "httpState": "Internal Server Error",
+                "errorData": str(e),
+            },
+        )
+
     return JSONResponse(
         status_code=200,
         content={"httpCode": 200, "httpState": "OK", "saldo": saldo},
@@ -77,20 +90,24 @@ async def registar_api(request: Request):
 
     cursor.execute("SELECT saldo FROM registos ORDER BY id DESC LIMIT 1")
     saldo_atual: float = cursor.fetchone()[0]
-
+    
+    """
     try:
-        if data["tipo"] == 1: # debito
+        if data["tipo"] == 1:  # debito
             cursor.execute(
-                "INSERT INTO registos (data, debito, saldo, descricao) VALUES (%s, %s, %s, %s)", (
-                    data["data"], data["valor"], saldo_atual - data["valor"], data["descricao"],
+                "INSERT INTO registos (data, debito, saldo, descricao) VALUES (%s, %s, %s, %s)",
+                (
+                    data["data"],
+                    data["valor"],
+                    saldo_atual - data["valor"],
+                    data["descricao"],
                 ),
             )
             db.commit()
-        elif data["tipo"] == 2: # credito
+        elif data["tipo"] == 2:  # credito
             cursor.execute(
-                "INSERT INTO registos (data, credito, saldo, descricao) VALUES (%s, %s, %s, %s)", (
-                    data["data"], data["valor"], saldo_atual + data["valor"], data["descricao"],
-                ),
+                "INSERT INTO registos (data, credito, saldo, descricao) VALUES (%s, %s, %s, %s)",
+                (data["data"], data["valor"], saldo_atual + data["valor"], data["descricao"], ),
             )
             db.commit()
     except Exception as e:
@@ -102,9 +119,57 @@ async def registar_api(request: Request):
                 "errorData": str(e),
             },
         )
+    """
 
     cursor.execute("SELECT id FROM registos ORDER BY id DESC LIMIT 1")
     return JSONResponse(
         status_code=200,
         content={"httpCode": 200, "httpState": "OK", "idRecord": cursor.fetchone()[0]},
+    )
+
+
+@app.post("/api/editar/{id}", response_class=JSONResponse)
+async def editar_api(id: int):
+    cursor.execute("SELECT * FROM registos WHERE id = %s", (id,))
+    data = cursor.fetchone()
+
+    return JSONResponse(
+        status_code=200,
+        content={"httpCode": 200, "httpState": "OK", "data": data},
+    )
+
+
+@app.post("/api/consulta", response_class=JSONResponse)
+async def consultar_api(request: Request):
+    data = await request.json()
+
+    try:
+        cursor.execute(
+            "SELECT * FROM registos WHERE `data` = %s", (data["dataConsulta"],)
+        )
+        registos = cursor.fetchall()
+
+        jsondata = []
+        for i in range(len(registos)):
+            jsondata.append({
+                    "id": registos[i][0],
+                    "data": str(registos[i][1]),
+                    "valor": str(registos[i][2]),
+                    "tipo": registos[i][3],
+                    "descricao": str(registos[i][4]),
+                })
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail={
+                "httpCode": 500,
+                "httpState": "Internal Server Error",
+                "errorData": str(e),
+            },
+        )
+
+    return JSONResponse(
+        status_code=200,
+        content={"httpCode": 200, "httpState": "OK", "data": jsondata},
     )
--- tabela.sql
+++ tabela.sql
@@ -1,11 +1,17 @@
 -- id data hora credito debito saldo descricao 
-
 CREATE TABLE IF NOT EXISTS `dashgastos`.`registos` (
     `id` INT NOT NULL AUTO_INCREMENT,
     `data` TEXT NOT NULL,
-    `credito` FLOAT NULL DEFAULT '0.0',
-    `debito` FLOAT NULL DEFAULT '0.0',
-    `saldo` FLOAT NULL DEFAULT '0.0',
-    `descricao` TEXT NOT NULL,
+    `valor` FLOAT NOT NULL DEFAULT '0.0',
+    `tipo` FLOAT NOT NULL DEFAULT '0.0',
+    `descricao` TEXT NOT NULL DEFAULT '',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
+
+-- get saldo
+SELECT ROUND(ABS(SUM(CASE WHEN tipo = 1 THEN valor ELSE 0 END) - SUM(CASE WHEN tipo = 2 THEN valor ELSE 0 END)), 2) AS saldo FROM registos;
+
+-- alter AUTO_INCREMENT of id
+ALTER TABLE `registos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
